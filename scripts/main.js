9e3ba2c65b2a40e02873c90c1f41bd8ce1050deb
Ashley Lewis <ashley.lewis@tracesmart.co.uk>
2012-10-05 20:04:05 +0100

    added prev. readings and update times

--- scripts/main.js
+++ scripts/main.js
@@ -10,17 +10,35 @@ require.config({
 });
  
 require(['app'], function(app) {
+
 	// use app here
 	app.success(function(response){
-		console.log(response);
 
-		$('.hero-unit h1').html(
-			'<span class="height">'+ response.item.readings.significantWaveHeight.replace(' ', '') +'</span>'+
+		var reading,
+			latestReadingTime,
+			nextReadingTime;
+		
+		reading = '<span class="height">'+ response.item.readings.significantWaveHeight.replace(' ', '') +'</span>'+
 			'<span> @ </span>'+
 			'<span class="period">'+ response.item.readings.averagePeriod.replace(' ', '') +'</span>'+
 			'<span>  w/ </span>'+
 			'<span class="wind-speed">'+ response.item.readings.windSpeed.replace(' knots', 'kts') +'</span>'+
-			'<span class="wind-direction"> '+ response.item.readings.windDirection.slice(0,-6) +'</span>'
+			'<span class="wind-direction"> '+ response.item.readings.windDirection.slice(0,-6) +'</span>';
+
+		latestReadingTime = new Date(response.item.pubDate);
+		nextReadingTime = new Date(latestReadingTime.getTime() + (1*60*60*1000));
+		
+		$('#latest-reading-time').html(latestReadingTime.toLocaleTimeString() +' '+ latestReadingTime.toLocaleDateString());
+		$('#next-reading-time').html(nextReadingTime.toLocaleTimeString() +' '+ nextReadingTime.toLocaleDateString());
+
+		$('#latest-reading').html(
+			reading
+		);		
+
+		$('#previous-readings').html(
+			'<ul><li>'+ reading +'</li>'+
+			'<li>'+ reading +'</li>'+
+			'<li>'+ reading +'</li></ul>'
 		);
 	});
 });
